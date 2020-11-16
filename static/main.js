54c8f9ab3a90c69a23b28dd61b7574e0c3c8fbda
Corey Fonseca <cfonseca@blueport.com>
2020-11-16 16:34:13 -0500

    POST to server works

--- static/main.js
+++ static/main.js
@@ -1,2 +1,17 @@
 var p = document.getElementById("test")
 p.innerText = "This is some text"
+
+submitButton = document.getElementById("submit")
+submitButton.onclick = function() {
+    var xhr = new XMLHttpRequest();
+
+    var fullURL = document.getElementById("fullURL");
+    var shortURL = document.getElementById("shortURL");
+    var data = JSON.stringify({ "fullURL": fullURL.value, "shortcut": shortURL.value }); 
+
+    xhr.open('POST', 'http://localhost:8000', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(data)
+
+    console.log("Sending data: " + data)
+}
