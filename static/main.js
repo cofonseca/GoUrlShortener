d00853ec07044893362f2d27f49f13e0e7ec3686
Corey Fonseca <cfonseca@blueport.com>
2021-08-12 10:17:02 -0400

    frontend form validation and onclick css changes

--- static/main.js
+++ static/main.js
@@ -3,31 +3,39 @@ const baseURL = window.location.href
 
 submitButton = document.getElementById("submit")
 submitButton.onclick = function() {
+    submitButton.classList.replace("pure-button-primary","pure-button-active")
     var xhr = new XMLHttpRequest();
     var fullURL = document.getElementById("fullURL");
     var shortURL = document.getElementById("shortURL");
     if (shortURL.value == "shortcut") {
         shortURL.value = ""
     }
-    var data = JSON.stringify({ "fullURL": fullURL.value, "shortcut": shortURL.value }); 
-    xhr.open('POST', baseURL, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
 
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status = 200) {
-                var response = JSON.parse(xhr.responseText)
-                if (response.Success == true) {
-                    result.innerHTML = ('Here\'s your link: ' + '<a href="' + baseURL + response.Shortcut + '">' + baseURL + response.Shortcut + '</a>')
+    if (fullURL.value == "") {
+        console.log("URL cannot be blank")
+        submitButton.classList.replace("pure-button-active","pure-button-primary")
+    } else {
+        var data = JSON.stringify({ "fullURL": fullURL.value, "shortcut": shortURL.value }); 
+        xhr.open('POST', baseURL, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status = 200) {
+                    var response = JSON.parse(xhr.responseText)
+                    if (response.Success == true) {
+                        result.innerHTML = ('Here\'s your link: ' + '<a href="' + baseURL + response.Shortcut + '">' + baseURL + response.Shortcut + '</a>')
+                    } else {
+                        result.innerHTML = ('Sorry, ' + shortURL.value + ' already exists! Try something else.')
+                    }
+                    
                 } else {
-                    result.innerHTML = ('Sorry, ' + shortURL.value + ' already exists! Try something else.')
+                    console.log("Error response from server")
                 }
-                
-            } else {
-                console.log("The server returned an error.")
             }
-        }
-      };
-    console.log("Sending data: " + data)
-    xhr.send(data)
+        };
+        console.log("Sending data: " + data)
+        xhr.send(data)
+    }
+    submitButton.classList.replace("pure-button-active","pure-button-primary")
 }   
